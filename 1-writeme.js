277a3c2f7f757e5d991d7c7745c63392836a4959
root <root@DESKTOP-I6FTCIN.localdomain>
2025-02-20 08:48:59 +0300

    done

---
+++ 1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+	const fs = require('fs');
+
+// Get arguments from the command line
+// const filePath = process.argv[2];
+// const text = process.argv[3];
+//
+// // Write the text to the file using utf-8 encoding
+// fs.writeFile(filePath, text, 'utf8', (err) => {
+//   if (err) {
+//       console.error(err);
+//         }
+//         });
